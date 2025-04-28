d526d68b3658c2de8afd20bd73542edfe9497337
DanielCrow65 <edgardo.b.gomez@gmail.com>
2025-04-28 12:52:01 +0800

    Created a sorting function and a proper-looking report screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import sort_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,10 +10,26 @@ def get_book_text(filepath):
 def main():
     book_path = "books/frankenstein.txt"
     book_content = get_book_text(book_path)
-    #test_string = "I am a test string, ya hoser...!"
-    print(f"{get_word_count(book_content)} words found in the document")
-    print(get_character_count(book_content))
+    book_word_count = get_word_count(book_content)
+    character_count = get_character_count(book_content)
+    sorted_character_count = sort_characters(character_count)
 
+    #test_string = "I am a test string, ya hoser...!"
+    #print(f"{book_word_count} words found in the document")
+    #print(character_count)
+    #print(sorted_character_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_word_count} total words")
+    print("--------- Character Count -------")
+    for c in sorted_character_count:
+        if c["char"].isalpha():
+            print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
+
+    #since this is a LIST of DICTIONARIES, read it as 'for each DICT in the LIST'. Thus c can use all dict-related functions
 
 main()
 
--- stats.py
+++ stats.py
@@ -13,3 +13,42 @@ def get_character_count(book_content):
         else:
             character_dict[char] = 1
     return character_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_characters(character_dict):
+    sorted_character_list = []
+
+    for char in character_dict:
+        temp_dict = {}
+        #if I declare this outside of the loop append will reference the first value I ever give it
+        #while in the loop, it is thrown away at the end of every iteration letting a fresh value be put in 
+        temp_dict["char"] = char
+        temp_dict["num"] = character_dict[char]
+        sorted_character_list.append(temp_dict)
+
+    sorted_character_list.sort(reverse=True, key=sort_on)
+    
+    return sorted_character_list
+"""   
+    character_keys = []
+    character_values = []
+    assigned_character_dict = {}
+
+    for char in character_dict:
+        character_keys.append(char)
+        character_values.append(character_dict[char])
+    
+    for k in character_keys:
+        assigned_character_dict["char"] = k
+    
+    for v in character_values:
+        assigned_character_dict["num"] = v
+
+    return assigned_character_dict
+"""    
+    #I was trying to create a single dictionary with all key-value pairs but since the keys do not have unique names 
+    #('char' and 'num'), the loop just overwrites them every time
+    #Boots advised me to instead create a LIST OF DICTIONARIES. Since each character is its own dictionary, no overwriting
+    #should happen!
