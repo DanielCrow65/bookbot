6d57171f18cd012efc4e264dd02320941fefc766
DanielCrow65 <edgardo.b.gomez@gmail.com>
2025-04-28 14:57:42 +0800

    Added a functionality to accept book names directly from a command line.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from stats import get_word_count
 from stats import get_character_count
 from stats import sort_characters
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = ""
+
+    #sys.argv is a LIST! Use List functions to interact with it!
+    #print("Testing sys inputs:", sys.argv) - diagnostic print statement, remove before submission
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+    
     book_content = get_book_text(book_path)
     book_word_count = get_word_count(book_content)
     character_count = get_character_count(book_content)
@@ -30,6 +42,7 @@ def main():
     print("============= END ===============")
 
     #since this is a LIST of DICTIONARIES, read it as 'for each DICT in the LIST'. Thus c can use all dict-related functions
+    #isalpha check prevents the program from including symbols. Alphanumeric stuff only!
 
 main()
 
